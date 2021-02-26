c7f075f4dc2ca5ccab90abe84ec255f7e0197fac
Guilherme Rocha <gleite98@gmail.com>
2021-02-26 16:27:08 -0300

    Finalizando área de desafios e iniciando implementação de contextos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import ExperienceBar from '../components/ExperienceBar';
 import {Profile} from '../components/Profile';
 import {CompletedChallenges} from '../components/CompletedChallenges';
 import {Countdown} from '../components/Countdown';
+import {ChallengeBox} from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
@@ -15,22 +16,23 @@ export default function Home() {
     <div className="styles.container"> 
 
     <Head>
-      <title>Início | Se Mexa</title>
+    <title>Início | Se Mexa</title>
     </Head>
 
     
     <ExperienceBar />
 
     <section>
-      <div>
-        <Profile />
-        <CompletedChallenges />
-        <Countdown />
-      </div>
+    <div>
+    <Profile />
+    <CompletedChallenges />
+    <Countdown />
 
-      <div>
-        
-      </div>
+    </div>
+
+    <div>
+    <ChallengeBox />
+    </div>
     </section>
 
     </div>
